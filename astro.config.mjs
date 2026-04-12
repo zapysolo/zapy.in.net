b46f4b6258b1a0e765ed0cca4e5340b25679b1b4
Zapy <zapy@Zapys-MacBook-Air.local>
2026-04-12 17:30:48 +0530

    updated astro version to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://zapy.in.net',
-  integrations: [tailwind(), mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx(), sitemap()],
 });
